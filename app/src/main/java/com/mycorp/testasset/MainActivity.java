e74b446dc38e9a29ad3700fa06b7b280d3b72141
iman Fauzi <imanfauzi29@gmail.com>
2020-08-13 09:44:18 +0700

    test

--- app/src/main/java/com/mycorp/testasset/MainActivity.java
+++ app/src/main/java/com/mycorp/testasset/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, MaterialLogin.class));
             }
         });
-        btn2.setOnClickListener(new View.OnClickListener() {
+        btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, MainCategory.class));
